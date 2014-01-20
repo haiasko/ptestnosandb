b7b45ad61e52fd5040b9bc7fdaa56f5786c1d9e1
Plamen Nikolaev <plamen@gmail.com>
2014-01-20 22:06:14 +0100

    replaced String with Text in the echo handler. What do i do when yesod says Build failed: Pausing... ?

--- Foundation.hs
+++ Foundation.hs
@@ -20,6 +20,10 @@ import Text.Jasmine (minifym)
 import Text.Hamlet (hamletFile)
 import Yesod.Core.Types (Logger)
 
+-- my imports
+
+import Data.Text
+
 -- | The site argument for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
 -- starts running, such as database connections. Every handler will have
